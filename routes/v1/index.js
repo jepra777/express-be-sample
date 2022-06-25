ea8dbbc7e2c84a9d49a384793b2b1b1b11010850
jepra777 <jere.prasetyo.jp@gmail.com>
2022-06-25 15:35:11 +0700

    Update BE

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -3,22 +3,22 @@ const v1 = require("express").Router()
 v1.get("/", (req,res) => res.send("JePra API Version 1.0"))
 v1.get("/users/", (req, res) => {
     res.status(200).json({
-        node: 1,
-        data: [
+        "node": 1,
+        "users": [
         {
-            name: "Jeremiah Prasetyo",
-            birthDate: "03 Januari 1999",
-            sex: "Man"
+            "name": "Jeremiah Prasetyo",
+            "birthDate": "03 Januari 1999",
+            "sex": "Man"
         },
         {
-            name: "Surya Prasetyo",
-            birthDate: "05 Maret 2027",
-            sex: "Man"
+            "name": "Surya Prasetyo",
+            "birthDate": "05 Maret 2027",
+            "sex": "Man"
         },
         {
-            name: "Bulan Prasetyo",
-            birthDate: "03 September 2028",
-            sex: "Woman"
+            "name": "Bulan Prasetyo",
+            "birthDate": "03 September 2028",
+            "sex": "Woman"
         }
     ]})
 })
